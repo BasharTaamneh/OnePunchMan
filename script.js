900dfca25017a2deaa7fc67239970e7289c325be
BasharTaamneh <>
2021-06-03 14:18:28 +0000

    activate webpage with js

---
+++ script.js
@@ -0,0 +1,30 @@
+var userName = "bashar";
+
+console.log("hi this is bashar here, lets start coding ^_^");
+
+alert("hello teacher assistant ^_^ this is " +userName)
+
+document.write("<p style='color:red;'> this is " + userName
+ + " from java script ^_^ </p>")
+
+var userAnswer = prompt("do you like anime ? ");
+console.log( userAnswer);
+
+var answer = "yes";
+var disanswer = "no";
+
+if (userAnswer == answer )
+ {
+
+  alert("you chose the best story")
+    
+}else if(userAnswer == disanswer)
+  {
+
+  alert("ok cheke out the serial story you will like it ^_^ ")
+  
+}
+else{
+  alert("rong answer !")
+}
+
