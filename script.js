771ba4df935df47611a5821498d1f06a6f532fda
BasharTaamneh <>
2021-06-06 11:30:28 +0000

    do while & for loop .functions with js

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 var userName = "bashar";
-
 console.log("hi this is bashar here, lets start coding ^_^");
-
-alert("hello teacher assistant ^_^ this is " +userName)
-
+alert("hello teacher assistant ^_^ this is " + userName)
 document.write("<p style='color:red;'> this is " + userName
- + " from java script ^_^ </p>")
-
+  + " from java script ^_^ </p>")
 var userAnswer = prompt("do you like anime ? ");
-console.log( userAnswer);
-
+console.log(userAnswer);
 var answer = "yes";
 var disanswer = "no";
-
-if (userAnswer == answer )
- {
-
+if (userAnswer == answer) {
   alert("you chose the best story")
-    
-}else if(userAnswer == disanswer)
-  {
-
+} else if (userAnswer == disanswer) {
   alert("ok cheke out the serial story you will like it ^_^ ")
-  
 }
-else{
+else {
   alert("rong answer !")
-  function confirm(y=".")
-   {
-  let x;
-  do 
-{
-    x = prompt(y);
-} while (!/y(es)?|no?/i.test(x));
-return /y(es)?/i.test(x);
-   }
-confirm("Mark a correct answer please");
+  function confirm(y = '') {
+    let x;
+    do {
+      x = prompt(y);
+    } while (!/y(es)?|no?/i.test(x));
+    return /y(es)?/i.test(x);
   }
-
+  confirm("Mark a correct answer please");
+}
+var n = prompt("how many punch men you want to see ?")
+var punmen = function () {
+  var output = Number;
+  var i = 0;
+  while (i < n) {
+    output += "<img src='https://i.pinimg.com/originals/0d/94/4d/0d944dc6b54ca7c06a5f0b85e0e770a3.jpg' />"
+    console.log(i, output)
+    i++;
+  }
+  return output;
+}
+var res = punmen();
+document.write(res);
