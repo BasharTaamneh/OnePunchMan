a354681097fc5d38dd62b2e23c7d0b4821017729
BasharTaamneh <>
2021-06-06 08:42:02 +0000

    edit code

--- script.js
+++ script.js
@@ -2,39 +2,36 @@ var userName = "bashar";
 
 console.log("hi this is bashar here, lets start coding ^_^");
 
-alert("hello teacher assistant ^_^ this is " +userName)
+alert("hello teacher assistant ^_^ this is " + userName)
 
 document.write("<p style='color:red;'> this is " + userName
- + " from java script ^_^ </p>")
+  + " from java script ^_^ </p>")
 
 var userAnswer = prompt("do you like anime ? ");
-console.log( userAnswer);
+console.log(userAnswer);
 
 var answer = "yes";
 var disanswer = "no";
 
-if (userAnswer == answer )
- {
+if (userAnswer == answer) {
 
   alert("you chose the best story")
-    
-}else if(userAnswer == disanswer)
-  {
+
+} else if (userAnswer == disanswer) {
 
   alert("ok cheke out the serial story you will like it ^_^ ")
-  
+
 }
-else{
+else {
   alert("rong answer !")
-  function confirm(y=".")
-   {
-  let x;
-  do 
-{
-    x = prompt(y);
-} while (!/y(es)?|no?/i.test(x));
-return /y(es)?/i.test(x);
-   }
-confirm("Mark a correct answer please");
+
+  function confirm(y = '') {
+    let x;
+    do {
+      x = prompt(y);
+    } while (!/y(es)?|no?/i.test(x));
+    return /y(es)?/i.test(x);
   }
+  confirm("Mark a correct answer please");
+}
 
